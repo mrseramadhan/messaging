f20034d33c07dfc41597b69dd4a2cd09d3e5e775
rahmat <rahmatputrawiranata@gmail.com>
2020-08-22 01:41:41 +0700

    test fix on server for 405

--- routes/api.go
+++ routes/api.go
@@ -8,6 +8,6 @@ import (
 
 func ApiRoutes(prefix string, r *mux.Router) {
 	s := r.PathPrefix(prefix).Subrouter()
-	s.HandleFunc("/messaging", controllers.CreateMessage).Methods("POST")
-	s.HandleFunc("/messaging/{id:[0-9]+}", controllers.SendMessage).Methods("GET")
+	s.HandleFunc("/messaging/create", controllers.CreateMessage).Methods("POST")
+	s.HandleFunc("/messaging/send/{id:[0-9]+}", controllers.SendMessage).Methods("GET")
 }
