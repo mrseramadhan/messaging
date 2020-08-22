f0363942d3152f0470d9f7243213f6a91d73a4b5
rahmat <rahmatputrawiranata@gmail.com>
2020-08-22 17:24:44 +0700

    another fix

--- main.go
+++ main.go
@@ -15,17 +15,7 @@ func startServer() {
 	port := os.Getenv("web_port")
 	prefix := os.Getenv("prefix")
 	fmt.Println("Server started at " + port + "...")
-	r := mux.NewRouter()
-	r.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Printf("OPTIONS")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
-		w.WriteHeader(http.StatusNoContent)
-		return
-	})
-
-	r.StrictSlash(true)
+	r := mux.NewRouter().StrictSlash(true)
 	// Routes
 	routes.ApiRoutes(prefix, r)
 
